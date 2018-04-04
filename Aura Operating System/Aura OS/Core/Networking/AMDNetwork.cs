c73980f8ad570214cd9a3d60d9128ec800bf5286
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-04-04 23:04:47 +0200

    Adding SetConfigIP()

---
+++ Aura Operating System/Aura OS/Core/Networking/AMDNetwork.cs
@@ -0,0 +1,51 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Get MAC Address
+* PROGRAMMERS:      Alexy DA CRUZ <dacruzalexy@gmail.com>
+*/
+
+using Cosmos.HAL;
+using Cosmos.HAL.Drivers.PCI.Network;
+
+namespace Aura_OS.Core.Networking
+{
+    class AMDNetwork
+    {
+        private static PCIDevice device = PCI.GetDevice(VendorID.AMD, DeviceID.PCNETII);
+
+        /// <summary>
+        /// Get MAC Address String
+        /// </summary>
+        public static string PhysicalAddress()
+        {
+            if (NetworkCardAvailable())
+            {
+                AMDPCNetII nic = new AMDPCNetII(device);
+                return nic.MACAddress.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static bool NetworkCardAvailable()
+        {
+            PCIDevice device;
+            device = PCI.GetDevice(VendorID.AMD, DeviceID.PCNETII);
+            if (device != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void SetConfigIP()
+        {
+
+        }
+    }
+}
