7c7f6740db19c2d2a4779e0d6968258a1db07913
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-04-05 20:46:37 +0200

    Adding Config class

---
+++ Aura Operating System/Aura OS/Core/Networking/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura_OS.Core.Networking
+{
+    class Config
+    {
+        public static string IP()
+        {
+            return null;
+        }
+
+    }
+}
