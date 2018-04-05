9092d784a8610ce9bb5834c2e2f2a57a4bc29b00
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-04-05 22:10:20 +0200

    Adding APIPA

---
+++ Aura Operating System/Aura OS/Core/Networking/APIPA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.System.Network.IPv4;
+
+namespace Aura_OS.Core.Networking
+{
+    class APIPA
+    {
+        public static void SetIP()
+        {
+            Random rnd = new Random();
+
+            int arg3 = rnd.Next(0, 256);
+            int arg4 = rnd.Next(0, 255);
+            byte third = (byte)arg3;
+            byte four = (byte)arg4;
+
+            Address address = new Address(169, 254, third, four);
+            Address subnet = new Address(255, 255, 0, 0);
+            Address zero = new Address(0, 0, 0, 0);
+
+            Netconfig.Set(AMDNetwork.AMDNetworkDevice() , address, subnet, zero);
+        }
+
+        public static void LookingForDHCP()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
