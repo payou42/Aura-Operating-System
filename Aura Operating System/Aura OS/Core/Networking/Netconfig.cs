1bd0eeafd2fd6d38337f7612911a3a298a78ff90
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2018-04-05 22:10:43 +0200

    Network framework

---
+++ Aura Operating System/Aura OS/Core/Networking/Netconfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Cosmos.System.Network;
+using Cosmos.System.Network.IPv4;
+
+namespace Aura_OS.Core.Networking
+{
+    class Netconfig
+    {
+        public static void Set(Cosmos.HAL.NetworkDevice nic, Address ip, Address subnet, Address gateway)
+        {
+            Config config = new Config(ip, subnet, gateway);
+            NetworkStack.ConfigIP(nic, config);
+        }
+
+    }
+}
